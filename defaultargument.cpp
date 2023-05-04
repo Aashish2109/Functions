9639d2aded94afa350a6c3fa290c0f33908067db
Aashish Gupta <99539876+Aashish2109@users.noreply.github.com>
2023-05-04 22:59:38 +0530

    default argument

---
+++ defaultargument.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int update(int a, int b = 3, int c = 3)
+{
+    int ans;
+    ans = a + b + c;
+    return ans;
+}
+int main()
+{
+    int a = 1;
+    cout << update(a);
+}
