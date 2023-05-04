d427a42bd9e7f415ad3be2dd7aaa2e86844116a0
Aashish Gupta <99539876+Aashish2109@users.noreply.github.com>
2023-05-04 23:00:16 +0530

    local variables

---
+++ local.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a = 2;
+int update(int a)
+{
+    int a1 = 2;
+    int ans;
+    return ans = a1 + a + 1;
+}
+int update2(int &a)
+{
+    int ans;
+    return ans = a;
+}
+int main()
+{
+    cout << update(a) << endl;
+    cout << update2(a) << endl;
+}
