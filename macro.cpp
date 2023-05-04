616f6411a41fc5d27826a8edbd1b5d75be5fe57a
Aashish Gupta <99539876+Aashish2109@users.noreply.github.com>
2023-05-04 23:00:23 +0530

    macros

---
+++ macro.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+const double PI = 3.14;
+int main()
+{
+    int radius;
+    cout << "Enter the radius of the circle:" << endl;
+    cin >> radius;
+    int ans;
+    ans = PI * (radius) * (radius);
+    cout << "The area of the Circle is:" << ans;
+}
